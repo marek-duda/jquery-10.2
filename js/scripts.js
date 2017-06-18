6b11569dd3aad1f23f8c4919e0f24977b4f29c80
Maro <grafika.marekduda@gmail.com>
2017-06-18 20:57:15 +0200

    deleted empty lines

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
 $('span').each(function(index, element) {
-	if(index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
+    if(index % 2 == 0) {
+        $(element).css('color', 'red');
+    };
 });
 
 $('p').each(function(index, element) {
-
     var button = $('<button/>').addClass('btn').attr({ 'data-tmp': index }).text('Click me');
     $(element).append(button); 
-
 });
 
 $('button').click(function(){
-	alert($(this).attr('data-tmp'));
+    alert($(this).attr('data-tmp'));
 });
